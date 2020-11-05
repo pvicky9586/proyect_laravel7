2076afd65571a59da54fd63cc894b48fc51de5b1
root <pvicky9586@gmail.com>
2020-11-05 03:52:07 -0400

    my 2do comic

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -9,13 +9,8 @@
 				<strong class="tite" align="center">Convencidos que los mejores tiempos <small class="tite2">son estos que vivimos. </small></strong>  
 			
 			<br>
-			<p>&nbsp;&nbsp;&nbsp;&nbsp;Miramos al porvenir como el tránsito laborioso de confeccionar el pan. 
-			Concientes que otra sociedad ha surgido.  </p>
-            	<details>
-					<summary><small style="color:#4D4D4D; font-size:14px;">Mas</small><img src="https://abogamur.es/wp-content/uploads/2019/06/abogados-de-familia-en-murcia.jpg"</summary>
-		            <p style="padding:5px;">Donde las fuerzas irruptivas van más allá del conocimiento formal. Mirando en cada entorno un valioso escenario formativo. Donde todos tenemos que desarrollar competencias y habilidades para insertarnos en ellos. Con la certeza de transformarlos para generar conexiones sensibles con el otro.
-				   </p> 
-				</details>
+			<p>TRBAJANDO CON LOS COMIT  </p>
+            	
 			 </div>    
 
 
