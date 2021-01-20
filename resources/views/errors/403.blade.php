c08aa404877973b2f185f4041fc566a1df45661f
Dsoft2020 <pvicky9586@gmail.com>
2021-01-19 23:00:58 -0400

    Delete resources/views/errors directory

--- resources/views/errors/403.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('errors::layout')
-
-@section('title', '403')
-@section('message', 'Pagina no encontrada')
--- resources/views/errors/404.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('errors::layout')
-
-@section('title', '404')
-@section('message', 'Pagina no encontrada')
